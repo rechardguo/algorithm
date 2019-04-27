3cb9de3fe72093fcd242e8b0c94b09b81cfb8e0c
reguo <guo_xiaolong@pactera.com>
2019-04-27 16:36:00 +0800

    合并2个链表

---
+++ src/main/java/rechard/learn/leecode/Q_21MergeTwoSortedLists.java
@@ -0,0 +1,32 @@
+package rechard.learn.leecode;
+
+import rechard.learn.algorithm.datastructure.ListNode;
+
+public class Q_21MergeTwoSortedLists {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode tmp=null;
+        ListNode root=new ListNode(0);
+        ListNode dummy=new ListNode(0);
+        dummy.next=root;
+        while(l1!=null||l2!=null){
+            if(l1!=null&&l2!=null){
+                if(l1.val<l2.val){
+                    tmp=l1;
+                    l1=l1.next;
+                }else{
+                    tmp=l2;
+                    l2=l2.next;
+                }
+            }else if(l1!=null){
+                tmp=l1;
+                l1=l1.next;
+            }else{
+                tmp=l2;
+                l2=l2.next;
+            }
+            root.next=tmp;
+            root=root.next;
+        }
+        return dummy.next.next;
+    }
+}
