dbf1073f5d1d6b0a6d5c64d8476fac0586dec1dd
reguo <guo_xiaolong@pactera.com>
2019-04-27 21:29:53 +0800

    找出最长的回文串，动态规划

---
+++ src/main/java/rechard/learn/leecode/Q_203RemoveLinkedListElements.java
@@ -0,0 +1,31 @@
+package rechard.learn.leecode;
+
+import rechard.learn.algorithm.datastructure.ListNode;
+
+/**
+ * 移除链表里的某个节点，对比Q_27
+ *
+ * 单链表的通性就是需要一个dummy节点
+ * dummy.next指向给定的head节点
+ *
+ * return的时候return dummy.next
+ *
+ */
+public class Q_203RemoveLinkedListElements {
+    public ListNode removeElements(ListNode head, int val) {
+        ListNode dummy=new ListNode(val==0?-1:-val);
+        dummy.next=head;
+        ListNode p =dummy,c=head;
+        while(c!=null){
+            if(c.val==val){
+                p.next=c.next;
+                c.next=null;
+                c=p;
+            }
+            p=c;
+            c=c.next;
+        }
+        return dummy.next;
+
+    }
+}
