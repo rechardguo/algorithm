93ca67c6bb651f081f6a016f7b4250439a798e8e
reguo <guo_xiaolong@pactera.com>
2019-05-07 00:10:24 +0800

    去除链表里的重复元素

---
+++ src/main/java/rechard/learn/leecode/Q_82RemoveDuplicatesfromSortedListII.java
@@ -0,0 +1,31 @@
+package rechard.learn.leecode;
+
+import rechard.learn.algorithm.datastructure.ListNode;
+
+public class Q_82RemoveDuplicatesfromSortedListII {
+
+    public ListNode deleteDuplicates(ListNode head) {
+        if(head==null)return null;
+        ListNode dummy=new ListNode(head.val-1);
+        dummy.next=head;
+        ListNode p=dummy;
+        ListNode c=p.next;
+        while(c!=null){
+            if(c.next==null){
+                break;
+            }
+            if(c.next.val==c.val){
+                ListNode tmp=c.next;
+                while(tmp!=null&&tmp.val==c.val){
+                    tmp=tmp.next;
+                }
+                p.next=tmp;
+                c=tmp;
+            }else{
+                p=c;
+                c=c.next;
+            }
+        }
+        return dummy.next;
+    }
+}
