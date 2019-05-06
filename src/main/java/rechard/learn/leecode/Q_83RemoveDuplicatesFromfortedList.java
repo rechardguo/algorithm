40e45a3ece570165fd5fe20e8c5a18fd721686eb
reguo <guo_xiaolong@pactera.com>
2019-05-06 23:22:00 +0800

    去除链表里重复的元素

---
+++ src/main/java/rechard/learn/leecode/Q_83RemoveDuplicatesFromfortedList.java
@@ -0,0 +1,43 @@
+package rechard.learn.leecode;
+
+import rechard.learn.algorithm.datastructure.ListNode;
+
+public class Q_83RemoveDuplicatesFromfortedList {
+
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode node=head;
+        while(node!=null){
+            if(node.next==null)
+                break;
+            if(node.val==node.next.val){
+                node.next=node.next.next;
+            }else{
+                node=node.next;
+            }
+        }
+        return head;
+    }
+
+
+    //虽然通过了，但是时间和空间复杂度都不高
+    public ListNode deleteDuplicates_my(ListNode head) {
+        if(head==null)return null;
+        ListNode dummy=new ListNode(-1);
+        dummy.next=head;
+        ListNode i=dummy.next;
+        ListNode j=i.next;
+        while(j!=null){
+            if(i.val==j.val){
+                while(j!=null&&j.val==i.val){
+                    j=j.next;
+                }
+                i.next=j;
+                i=j;
+            }else{
+                i=j;
+                j=j.next;
+            }
+        }
+        return dummy.next;
+    }
+}
