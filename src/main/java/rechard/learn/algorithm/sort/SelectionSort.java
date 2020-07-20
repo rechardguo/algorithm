0893a21e01f2df9c098fc0ed007c2aabc5b5d22b
rechard-guo <sdragonguo@163.com>
2020-07-20 14:30:17 +0800

    1.添加注释 2.改用 junit test来验证
    Signed-off-by: rechard-guo <sdragonguo@163.com>

--- src/main/java/rechard/learn/algorithm/sort/SelectionSort.java
+++ src/main/java/rechard/learn/algorithm/sort/SelectionSort.java
@@ -1,11 +1,29 @@
 package rechard.learn.algorithm.sort;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 /**
  * 选择排序
  */
     public class SelectionSort {
 
-        public void sort(int[] arr){
+    @Test
+    public  void check(){
+        for(int i=0;i<100;i++) {
+            int[] arr = SortUtil.generateArr(i);
+            int[] arr2 = SortUtil.clone(arr);
+            sort(arr);
+            BubboSort.sort(arr2);
+            Assert.assertArrayEquals(arr, arr2);
+        }
+    }
+
+    /**
+     * 左边是有序的，i为有序的边界，每次从右边选择最小的元素,然后和i交换位置后i++
+      * @param arr
+     */
+    public void sort(int[] arr){
             for (int i = 0; i <arr.length ; i++) {
                 int min=i;
                 for(int j=i;j<arr.length;j++){
@@ -19,15 +37,4 @@ package rechard.learn.algorithm.sort;
             }
         }
 
-    public static void main(String[] args) {
-        int [] arr =SortUtil.generateArr(100);
-        for (int i=0;i<arr.length;i++){
-            System.out.print(arr[i]+" ");
-        }
-        System.out.println("\r\n=============================");
-        new SelectionSort().sort(arr);
-        for (int i=0;i<arr.length;i++){
-            System.out.print(arr[i]+" ");
-        }
-    }
 }
