02982163f6a013057eae3e332a31048773c63b99
rechard-guo <sdragonguo@163.com>
2021-02-22 13:32:00 +0800

    2021/2/22 二叉树练习
    Signed-off-by: rechard-guo <sdragonguo@163.com>

--- src/main/java/leetcode/editor/cn/[100]相同的树.java
+++ src/main/java/leetcode/editor/cn/[100]相同的树.java
@@ -53,13 +53,18 @@ class SameTree{
  *     TreeNode(int x) { val = x; }
  * }
  */
-class Solution {
+    /**
+     * 本质上是前序遍历
+     */
+    class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        if(p==null && q==null)return true;
-        if(p==null || q==null)return false;
-        if(p.val!=q.val) return false;
-
-        return isSameTree(p.left,q.left)&&isSameTree(p.right,q.right);
+       if(p==null && q==null)return true;
+       if((p!=null && q==null)||(p==null && q!=null))return false;
+       if(p!=null && q!=null){
+           if(p.val!=q.val)
+               return false;
+       }
+       return isSameTree(p.left,q.left) && isSameTree(p.right,q.right);
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
