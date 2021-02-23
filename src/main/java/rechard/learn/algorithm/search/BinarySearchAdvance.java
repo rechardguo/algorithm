8f60789e9b8e76c80ba032a3f519e9bf20dfce35
rechard-guo <sdragonguo@163.com>
2021-02-23 14:57:03 +0800

    复习了下二分查找的写法 分别查找了右侧和左侧边界
    Signed-off-by: rechard-guo <sdragonguo@163.com>

--- src/main/java/rechard/learn/algorithm/search/BinarySearchAdvance.java
+++ src/main/java/rechard/learn/algorithm/search/BinarySearchAdvance.java
@@ -33,39 +33,72 @@ public class BinarySearchAdvance {
 
 	public int[] binarysearch(int []arr ,int val){
 		if(arr==null) return null;
-		int left =left(arr,val);
-		int right=right(arr,val);
+		int left =left_bound(arr,val);
+		int right=right_bound(arr,val);
 		if(left==right)
 		  return new int[]{left,right};
 		else
 			return left==-1? new int[]{right,right}:right==-1?new int[]{left,left}:new int[]{left,right};
 	}
-
-
-
-	public int left(int[]arr ,int val){
-		int i=0,j=arr.length-1;
-		while(i<j){
-			int mid= i+(j-i>>1);
-
-			if(arr[mid]<val){
-				i=mid+1;
-			}else if(arr[mid]>val){
-				j=mid-1;
-			}else{
-				// 1,2,2,2,1
-				// i   j
-				if(j==mid)break;
-				j=mid;
+	// 1 2 2 2 2 3
+	// l   m     r
+	// l r
+	int left_bound(int[] nums, int target) {
+		int left = 0, right = nums.length - 1;
+		while (left <= right) {
+			int mid = left + (right - left) / 2;
+			if (nums[mid] < target) {
+				left = mid + 1;
+			} else if (nums[mid] > target) {
+				right = mid - 1;
+			} else if (nums[mid] == target) {
+				// 别返回，锁定左侧边界
+				right = mid - 1;
 			}
 		}
-		if(arr[i]==val)return i;
-		if(arr[j]==val)return j;
+		// 最后要检查 left 越界的情况
+		if (left >= nums.length || nums[left] != target)
+			return -1;
+		return left;
+	}
+	int right_bound(int[] nums, int target) {
+		int left = 0, right = nums.length - 1;
+		while (left <= right) {
+			int mid = left + (right - left) / 2;
+			if (nums[mid] < target) {
+				left = mid + 1;
+			} else if (nums[mid] > target) {
+				right = mid - 1;
+			} else if (nums[mid] == target) {
+				// 别返回，锁定右侧边界
+				left = mid + 1;
+			}
+		}
+		// 最后要检查 right 越界的情况
+		if (right < 0 || nums[right] != target)
+			return -1;
+		return right;
+	}
+/*
+
+	public int left(int[]arr ,int val){
+		int left=0,right=arr.length-1;
+		while(left<=right){
+			int mid=left+(right-left>>1);
+			if(arr[mid]<val){
+				left=mid+1;
+			}else if(arr[mid]>val){
+				right=mid-1;
+			}else{
+				left=mid+1;   // 别返回，锁定右侧边界
+			}
+		}
+		if(right>=arr.length)
 		return -1;
 	}
 	public int right(int[]arr ,int val){
 		int i=0,j=arr.length-1;
-		while(i<j){
+		while(i<=j){
 			int mid= i+(j-i>>1);
 
 			if(arr[mid]<val){
@@ -82,7 +115,7 @@ public class BinarySearchAdvance {
 		if(arr[j]==val)return j;
 		if(arr[i]==val)return i;
 		return -1;
-	}
+	}*/
 
 
 
